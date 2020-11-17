0801695f8ac02bee46d0f05483e14722f2a7869e
mengxiao <jkl1234560789@qq.com>
2020-11-17 11:30:25 +0800

    测试

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // Register the extension.
-Dcat\Admin\Admin::extend(Dcat\Admin\Extension\VillageDistpicker\VillageDistpicker::class);
+//Dcat\Admin\Admin::extension(Dcat\Admin\Extension\VillageDistpicker\VillageDistpicker::class);
 
 Dcat\Admin\Grid\Filter::extend('village_distpicker', \Dcat\Admin\Extension\VillageDistpicker\DistpickerFilter::class);
 
